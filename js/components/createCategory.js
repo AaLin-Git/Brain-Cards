59e8c552af79b9f16796f54f41d849b34d21aa9a
AaLin-Git <aalin.box.it@gmail.com>
2023-04-20 00:05:38 +0200

    feat: add createCategoryCard and editCategory

--- js/components/createCategory.js
+++ js/components/createCategory.js
@@ -1,43 +1,71 @@
-import { createElement } from "../helper/createElement";
+import { createElement } from '../helper/createElement.js';
+import { declOfNum } from '../helper/declOfNum.js';
 
-export const createCategory = (app) => {
-  const category = createElement("section", {
-    className: "category section-offset",
+export const createCategory = app => {
+  const category = createElement('section', {
+    className: 'category section-offset',
   });
 
-  const container = createElement("div", {
-    className: "container",
+  const container = createElement('div', {
+    className: 'container',
   });
 
   category.append(container);
 
-  const categoryList = createElement("ul", {
-    className: "category__list",
+  const categoryList = createElement('ul', {
+    className: 'category__list',
   });
 
-  const createCategoryCards = (data) => {
-    const item = createElement("li", {
-      className: "category__item",
-      textContent: data.title,
+  container.append(categoryList);
+
+  const createCategoryCards = data => {
+    const item = createElement('li', {
+      className: 'category__item',
     });
 
     item.dataset.id = data.id;
 
+    const btnCard = createElement('button', {
+      className: 'category__card',
+    });
+
+    const titleText = createElement('span', {
+      className: 'category__title',
+      textContent: declOfNum(data.length, ['пара', 'пары', 'пар']),
+    });
+
+    const countPairs = createElement('span', {
+      className: 'category__pairs',
+      textContent: data.title,
+    });
+
+    btnCard.append(titleText, countPairs);
+
+    const btnEdit = createElement('button', {
+      className: 'category__btn category__edit',
+      ariaLabel: 'редактировать',
+    });
+
+    const btnDel = createElement('button', {
+      className: 'category__btn category__del',
+      ariaLabel: 'удалить',
+    });
+
+    item.append(btnCard, btnEdit, btnDel);
+
     return item;
   };
 
-  container.append(ul);
-
-  const mount = () => {
-    categoryList.textContent = "";
-    app.append(category);
+  const mount = data => {
+    categoryList.textContent = '';
     const cards = data.map(createCategoryCards);
     categoryList.append(...cards);
+    app.append(category);
   };
 
   const unmount = () => {
     category.remove();
-  }
+  };
 
-  return { mount, unmount, categoryList }
+  return { mount, unmount, categoryList };
 };
--- js/service/api.service.js
+++ js/service/api.service.js
@@ -1,8 +1,8 @@
-const API_URL = "https://chief-foggy-troodon.glitch.me/";
+const API_URL = 'https://chief-foggy-troodon.glitch.me/';
 
 export const fetchCategories = async () => {
   try {
-    const response = await fetch(`${API_URL}/api/category`);
+    const response = await fetch(`${API_URL}api/category`);
 
     if (!(response.status === 200 || response.status === 201)) {
       const error = await response.json();
@@ -11,6 +11,22 @@ export const fetchCategories = async () => {
 
     const categories = await response.json();
     return categories;
+  } catch (error) {
+    return { error };
+  }
+};
+
+export const fetchCards = async id => {
+  try {
+    const response = await fetch(`${API_URL}api/category/${id}`);
+
+    if (!(response.status === 200 || response.status === 201)) {
+      const error = await response.json();
+      throw error;
+    }
+
+    const cards = await response.json();
+    return cards;
   } catch (error) {
     return { error };
   }
